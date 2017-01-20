cd895ca34e34d3f4ce82e2a7488968038cedb5c8
doctor3397 <hs2546@columbia.edu>
2017-01-20 17:12:41 -0500

    Contact Storage System

--- contact.rb
+++ contact.rb
@@ -1,58 +1,178 @@
 class Contact
 
-  # This method should initialize the contact's attributes
-  def initialize
+  # attr_accessor will give you both getters and setters,
+  # attr_reader gives you just getters, and
+  # attr_writer gives you just setters.
+  attr_accessor :first_name, :last_name, :email, :note
+  attr_reader :id
+  #
+  # def email
+  #   @email
+  # end
+  #
+  # def email=(new_email)
+  #   @email= new_email
+  # end
 
+
+  #   Class variables
+  # A class variable is a variable that's declared at the class level and shared across all objects of the same type. In our case, we'll setup a class variable to store the array of all the contacts we create. We'll also create a class variable called @@id to ensure that our contacts each have a unique identifier.
+  @@contacts = []
+  @@id = 1
+
+  # Our initialize method should be responsible for setting the first name, last name, email, and note that get passed in from the create method.
+
+  # This method should initialize the contact's attributes
+  def initialize(first_name, last_name, email, note)
+    @first_name = first_name # store in the instance variables
+    @last_name = last_name
+    @email = email
+    @note = note
+
+    # Additionally, it should set the id of the contact and increment the class @@id variable so that the next contact will get a different id.
+    @id = @@id
+    #@@id += 1 # this way the next contact will get a different id
+
+    # @@contacts << self # self == The instance that was initialized
   end
 
-  # This method should call the initializer, 
-  # store the newly created contact, and then return it
-  def self.create
+  # Add a method to create new Contacts
+  # Adding a contact to our CRM has two steps:
+  # 1. first we need to make ("instantiate") a new contact object
+  # 2. and then we need to save it into the list of contacts.
+  # We want to keep our methods focused and simple so we'll separate the functionality of instantiating an object from the functionality of adding it to our list with a new class method.
 
+  # This method should call the initializer,
+  # store the newly created contact, and then return it
+  def self.create(first_name, last_name, email, note)
+    # 1. Initialize a new Contact with a unique ID
+    new_contact = self.new(first_name, last_name, email, note)
+    #new_contact = Contact.new(first_name, last_name, email, note)
+
+    # 2. Add the new Contact to the contacts list
+    @@contacts << new_contact
+
+    # 3. increment the next unique ID
+    @@id += 1
+
+    # 4. Return the newly created contacts
+    new_contact
   end
 
   # This method should return all of the existing contacts
-  def self.all
-
+  def self.all #self == Contact class
+    @@contacts
   end
 
   # This method should accept an id as an argument
   # and return the contact who has that id
-  def self.find
-
+  def self.find(id)
+    @@contacts.each do |contact|
+      if contact.id == id
+        return contact
+      end
+    end
   end
 
-  # This method should allow you to specify 
+  #  Instance methods should involve logic that only make sense being applied to an instance of something, such as updating the email of a specific person. For example, be some_contact.update_email(email)
+
+  # This method should allow you to specify
   # 1. which of the contact's attributes you want to update
   # 2. the new value for that attribute
   # and then make the appropriate change to the contact
-  def update
+  def update(attribute, value)
 
+    case attribute
+      when "first_name"
+        self.first_name = value
+      when "last_name"
+        self.last_name = value
+      when "email"
+        self.email = value
+      when "note"
+        self.note = value
+    end
+
+    # send("#{attribute}=", value)
   end
+
+  # A class method involves logic that should be applied on the whole scale of the model, such as cleaning up the email addresses of all the contacts. For example, Contact.clean_email_addresses.
 
   # This method should work similarly to the find method above
   # but it should allow you to search for a contact using attributes other than id
   # by specifying both the name of the attribute and the value
   # eg. searching for 'first_name', 'Betty' should return the first contact named Betty
-  def self.find_by
+  def self.find_by(attribute, value)
+    results = []
+    @@contacts.each do |contact|
 
+      case attribute
+        when "first_name"
+          if contact.first_name.downcase.include?(value)
+            results << contact
+          end
+        when "last_name"
+          if contact.last_name.downcase.include?(value)
+            results << contact
+          end
+        when "email"
+          if contact.email.downcase.include?(value)
+            results << contact
+          end
+        when "note"
+          if contact.note.downcase.include?(value)
+            results << contact
+          end
+      end
+    end
+
+    return results
   end
 
   # This method should delete all of the contacts
   def self.delete_all
-
+    @@contacts = []
   end
 
   def full_name
-
+    "#{first_name} #{last_name}" # Method calls
   end
 
   # This method should delete the contact
   # HINT: Check the Array class docs for built-in methods that might be useful here
   def delete
-
+    @@contacts.delete(self)
   end
 
-  # Feel free to add other methods here, if you need them.
-  
 end
+
+
+# Now whenever we want to make a new contact, we could do something like this:
+# betty = Contact.create('Betty', 'Maker', 'bettymakes@gmail.com', 'Loves Pokemon')
+# fred = Contact.create('Fred', 'Ngo', 'fred@gmail.com', 'Pizza')
+
+# p betty.full_name # => 'bettymakes@gmail.com'
+# p betty.note #=> 'Loves HTML & CSS'
+# p betty.first_name
+# p betty.full_name
+# p fred.full_name
+
+# p Contact.all
+# p betty.id
+
+# p Contact.find(1)
+# betty.update("email", "betty@hotmail.com")
+# p betty.email
+#
+# fred.update("note", "Yum")
+# p fred.note
+# betty.email = "betty@hotmail.com"
+# p betty.email
+
+#p Contact.find_by("last_name","Ngo")
+
+# betty.delete
+# p Contact.all
+
+# Contact.delete_all
+# p Contact.all
--- crm.rb
+++ crm.rb
@@ -1,40 +1,138 @@
+# Note that the CRM class needs to makes use of the Contact class, for example in the add_new_contact method. Many of the other methods in the CRM class will also make use of the Contact class.
+
+# Classes work together to make the entire program functional. We let classes use other classes by importing a file. The way this is done is using a method called require_relative, and basically what it does is get that file and stick it right in that line where the require_relative method was called.
+
+require './contact'
+
 class CRM
 
-  def initialize
+  # When we want to create a new instance of a class, we call a method named new (as in CRM.new). In some cases we want it so that arguments have to be passed in, for example the new instance you make has to be given a name. In other words, you want to see something like this: CRM.new("My CRM App").
 
+  # For this to be done we need to manipulate a method named initialize in the class.
+  # initialize and new are synonymous, so when you call new, you're calling initialize.
+  def initialize(name)
+    @name = name
   end
 
+  # Prompt the user to select from a main menu. The code for that might look something like this:
+  # We have two methods: print_main_menu prints out the menu and main_menu calls print_main_menu and then stores the user input (a number) into a variable called user_selected. That variable then gets passed as an argument into a method we haven't defined yet called call_option.
   def main_menu
-
+    while true # repeat indefinitely
+      print_main_menu
+      user_selected = gets.to_i
+      call_option(user_selected)
+    end
   end
 
   def print_main_menu
+    puts '[1] Add a new contact'
+    puts '[2] Modify an existing contact'
+    puts '[3] Delete a contact'
+    puts '[4] Display all the contacts'
+    puts '[5] Search by attribute'
+    puts '[6] Exit'
+    puts 'Enter a number: '
+  end
+
+  # user_selected is going to be a number. What we want to do in this method is call other methods based on the number. We've started off this method for you:
+  def call_option(user_selected)
+    case user_selected
+      when 1 then add_new_contact
+      when 2 then modify_existing_contact
+      when 3 then delete_a_contact
+      when 4 then display_all_contacts
+      when 5 then search_by_attribute
+      when 6 then exit
+    end
 
   end
 
-  def call_option
-
-  end
-
+  # The add_new_contact method might looks something like this:
   def add_new_contact
+    print 'Enter First Name: '
+    first_name = gets.chomp
 
+    print 'Enter Last Name: '
+    last_name = gets.chomp
+
+    print 'Enter Email Address: '
+    email = gets.chomp
+
+    print 'Enter a Note: '
+    note = gets.chomp
+
+    c = Contact.create(first_name, last_name, email, note)
+    puts "Name: #{c.full_name} Email: #{c.email} Note: #{c.note} was created"
   end
 
   def modify_existing_contact
+    display_all_contacts
 
+    print 'Enter the ID of the contact to modify: '
+    id = gets.chomp.to_i
+    c = Contact.find(id)
+    display_a_contact(c)
+
+    p "Enter an attribute to modify:  "
+    print "Enter: First name, Last Name, Email and Note "
+    attribute = gets.chomp.downcase.split(" ").join("_").to_s
+
+    print "Enter a new value: "
+    value = gets.chomp.to_s
+    c.update(attribute, value)
+
+    display_a_contact(c)
   end
 
-  def delete_contact
+  def delete_a_contact
+
+    print 'Enter the ID of the contact to delete: '
+    id = gets.chomp.to_i
+    c = Contact.find(id)
+    display_a_contact(c)
+    print "Are you sure you would like to delete?(y/n) "
+    answer = gets.chomp.downcase.to_s
+    if answer == "y"
+      c.delete
+    else
+      main_menu
+    end
 
   end
 
   def display_all_contacts
-    
+    all_c = Contact.all
+    all_c.each do |c|
+      puts "ID: #{c.id} Name: #{c.full_name} Email: #{c.email} Note: #{c.note}"
+    end
+  end
+
+  def display_a_contact(c)
+    puts "ID: #{c.id} Name: #{c.full_name} Email: #{c.email} Note: #{c.note}"
   end
 
   def search_by_attribute
-    
+    print "Enter an attribute you would like to search: First name, Last Name, Email and Note "
+    attribute = gets.chomp.downcase.split(" ").join("_").to_s
+
+    print "Enter the search value: "
+    value = gets.chomp.downcase.to_s
+    contacts = Contact.find_by(attribute, value)
+
+    p "Here is the search result"
+    contacts.each do |contact|
+      display_a_contact(contact)
+    end
   end
 
-
 end
+
+# Now how do we call methods that are inside of classes? Like this:
+# We're creating a new instance of the class CRM, and we're saving that new instance into a variable called a_crm_app. This is just like having a blueprint of a house, and every time you build a house off the blueprint you create a new instance of a house.
+
+# Then we can call the methods that we defined inside the class on the instance. main_menu and print_main_menu are methods inside of the class CRM, and we can call them on a_crm_app, which is an instance of the class. For example, a_crm_app.main_menu tells a_crm_app to perform the main_menu action.
+crm_app = CRM.new("CRM APP")
+crm_app.main_menu
+
+# If we wanted to call print_main_menu instead, that would look like:
+#crm_app.print_main_menu
